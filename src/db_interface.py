5ef7b254b7326ed653629e48a27614dda85f1542
theUltimateZoltan <eladlevysignups@gmail.com>
2022-02-15 22:52:04 +0200

    managed to get a replica set mongoDB running with a pymongo driver

--- src/db_interface.py
+++ src/db_interface.py
@@ -1,3 +1,33 @@
-class DoesNothing:
-    def do_nothing(self):
+import enum
+from pymongo import MongoClient
+
+
+class MongoWrapper:
+    class Collection(enum.Enum):
+        Test = "test_collection"
+
+    def __init__(self, collection: Collection):
+        self.__db_name = "test_app_db"
+        self.__client = MongoClient("mongo_master", replicaset="dbrs")
+        self.__collection = self.__client[self.__db_name][collection.value]
+
+    def __repr__(self):
+        return f"{self.__collection}"
+
+    def insert(self, json_object):
+        self.__collection.insert_one(json_object)
+
+    def contains(self, json_object) -> bool:
+        return bool(self.__collection.find_one(json_object))
+
+    def delete_everything(self):
         pass
+
+    def close(self):
+        self.__client.close()
+
+
+if __name__ == '__main__':
+    test_collection = MongoWrapper(collection=MongoWrapper.Collection.Test)
+    test_collection.insert({"a": 1, "b": 2})
+    test_collection.close()
--- test/test_db_interface.py
+++ test/test_db_interface.py
@@ -1,6 +1,55 @@
-import pytest
-from db_interface import DoesNothing
+from pytest import fixture
+from db_interface import MongoWrapper
 
-def test_nothing():
-    n = DoesNothing()
-    n.do_nothing()
+
+@fixture(scope="module",  autouse=True)
+def faker_seed():
+    return 512
+
+
+@fixture
+def sample_size():
+    return 12
+
+
+@fixture
+def sample_data(faker, sample_size):
+    return [{
+        "name": faker.name(),
+        "long_text": faker.paragraph(),
+        "age": faker.pyint(12, 99),
+        "some_date": faker.date(),
+        "some_time": faker.time(),
+        "some_boolean": faker.pybool()
+    } for _ in range(sample_size)]
+
+
+@fixture
+def collection():
+    test_collection = MongoWrapper(collection=MongoWrapper.Collection.Test)
+    return test_collection
+
+
+@fixture(autouse=True)
+
+
+def setup_teardown(collection):
+    yield
+    collection.delete_everything()
+    collection.close()
+
+
+def test_insert_then_found(sample_data, collection):
+    collection.insert(sample_data[0])
+    assert collection.contains(sample_data[0])
+
+
+def test_find_not_inserted(sample_data, collection):
+    collection.insert(sample_data[0])
+    assert not collection.contains(sample_data[1])
+
+
+def test_data_persistent(sample_data, collection):
+    collection.insert(sample_data[0])
+    another_conn = MongoWrapper(MongoWrapper.Collection.Test)
+    assert another_conn.contains(sample_data[0])
