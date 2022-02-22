7966c6a2a99457d71fe00d03bfd9b5e1530b2297
theUltimateZoltan <eladlevysignups@gmail.com>
2022-02-22 23:39:11 +0200

    added more basic functionality

--- src/db_interface.py
+++ src/db_interface.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+
 
 DB_NAME = "app_data"
 DB_PORT = 27017
@@ -11,8 +13,17 @@ class MongoCollectionInterface:
         self.__collection = self.__client[DB_NAME][collection]
 
     @staticmethod
-    def __default_client():
+    def __default_client() -> MongoClient:
         return MongoClient(DB_HOST, DB_PORT, replicaset=DB_REPLICA_SET)
 
-    def insert(self, record) -> None:
+    def insert(self, record) -> ObjectId:
         return self.__collection.insert_one(record).inserted_id
+    
+    def delete_by_id(self, record_id: ObjectId) -> None:
+        self.__collection.delete_one({'_id': record_id})
+
+    def update_by_id(self, record_id: ObjectId, updated_fields:dict) -> None:
+        self.__collection.update_one({'_id': record_id}, {"$set": updated_fields})
+
+    def get_first_id_for_properties(self, properties_to_search) -> ObjectId:
+        return self.__collection.find_one(properties_to_search)["_id"]
--- test/test_db_interface.py
+++ test/test_db_interface.py
@@ -12,6 +12,26 @@ def test_connection(fake_client):
 def test_insert(fake_client):
     mongo = MongoCollectionInterface("test", fake_client)
     id = mongo.insert({"name": "bob"})
-    result=fake_client[DB_NAME]["test"].find_one(id)
+    result=fake_client[DB_NAME]["test"].find_one({"_id": id})
     assert result
     assert result["name"] == "bob"
+
+def test_delete_by_id(fake_client):
+    mongo = MongoCollectionInterface("test", fake_client)
+    id = fake_client[DB_NAME]["test"].insert_one({"name": "bob"}).inserted_id
+    mongo.delete_by_id(id)
+    result = fake_client[DB_NAME]["test"].find_one(id)
+    assert result is None
+
+def test_update_by_id(fake_client):
+    mongo = MongoCollectionInterface("test", fake_client)
+    id = fake_client[DB_NAME]["test"].insert_one({"name": "bob"}).inserted_id
+    mongo.update_by_id(id, {"name": "alice", "age": 23})
+    result = fake_client[DB_NAME]["test"].find_one(id)
+    assert result["name"] == "alice" and result["age"] == 23
+
+def test_get_id_for_properties(fake_client):
+    mongo = MongoCollectionInterface("test", fake_client)
+    id = fake_client[DB_NAME]["test"].insert_one({"name": "bob"}).inserted_id
+    retrieved_id = mongo.get_first_id_for_properties({"name": "bob"})
+    assert retrieved_id == id
